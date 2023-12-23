694eb43c6722559c1ae130e4f44ad5beba8e5d92
luxq <xueqian1991@gmail.com>
2023-12-23 15:12:41 +0000

    4 node success

--- generate.sh
+++ generate.sh
@@ -17,9 +17,18 @@ rm -f $file
 echo 'version: "3.9"' >> $file
 echo '' >> $file
 echo 'services:' >> $file
-echo '  execution:' >> $file
+
+
+for idx in `seq 1 $node_count`
+do
+	num=$step
+	index=$(( $num * ($idx-1)))
+#execution config
+geth_rpc_port=$((1000+$idx))
+geth_grpc_port=$((2000+$idx))
+echo "  execute${idx}:" >> $file
 echo '    image: ethnet/tools' >> $file
-echo '    container_name: execute' >> $file
+echo "    container_name: execute${idx}" >> $file
 echo '    entrypoint: /usr/local/bin/execution.sh' >> $file
 echo '    deploy:' >> $file
 echo '      restart_policy:' >> $file
@@ -28,18 +37,16 @@ echo '        delay: 1s' >> $file
 echo '        max_attempts: 100' >> $file
 echo '        window: 120s' >> $file
 echo '    ports:' >> $file
-echo '      - "8551:8551"' >> $file
+echo "      - \"$geth_grpc_port:8551\"" >> $file
+echo "      - \"$geth_rpc_port:8545\"" >> $file
 echo '    volumes:' >> $file
 echo '      - ./config:/root/config' >> $file
-echo '      - ./data/gethdata:/root/gethdata' >> $file
+echo "      - ./data/gethdata${idx}:/root/gethdata" >> $file
 echo '    networks:' >> $file
 echo '      - meta' >> $file
 echo '' >> $file
 
-for idx in `seq 1 $node_count`
-do
-	num=$step
-	index=$(( $num * ($idx-1)))
+# beacon chain config
 echo "  beacon${idx}:" >> $file
 echo "    image: ethnet/tools" >> $file
 echo "    container_name: beacon${idx}" >> $file
@@ -47,6 +54,7 @@ echo "    entrypoint: /usr/local/bin/beacon-node.sh" >> $file
 echo "    environment:" >> $file
 echo '      - BOOTNODE=${BOOTNODE_ENR}' >> $file
 echo '      - ALLPEERS=${ALLPEERS}' >> $file
+echo "      - EXECUTE=execute${idx}" >> $file
 echo "    deploy:" >> $file
 echo "      restart_policy:" >> $file
 echo "        condition: on-failure" >> $file
@@ -57,10 +65,11 @@ echo "    volumes:" >> $file
 echo "      - ./config:/root/config" >> $file
 echo "      - ./data/beacon${idx}:/root/beacondata" >> $file
 echo "    depends_on:" >> $file
-echo "      - execution" >> $file
+echo "      - execute${idx}" >> $file
 echo "    networks:" >> $file
 echo "      meta:" >> $file
 echo "        ipv4_address: 172.99.1.${idx}" >> $file
+
 echo "" >> $file
 echo "  validators${idx}:" >> $file
 echo '    image: ethnet/tools' >> $file
@@ -80,7 +89,6 @@ echo '    volumes:' >> $file
 echo '      - ./config:/root/config' >> $file
 echo "      - ./data/validators${idx}:/root/validatordata" >> $file
 echo '    depends_on:' >> $file
-echo '      - execution' >> $file
 echo "      - beacon${idx}" >> $file
 echo '    networks:' >> $file
 echo '      - meta' >> $file
@@ -94,3 +102,4 @@ echo '      config:' >> $file
 echo '        - subnet: 172.99.0.0/16' >> $file
 
 ./clear.sh
+./genesis.sh
--- scripts/beacon-node.sh
+++ scripts/beacon-node.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-echo "beacon node with bootnode=$BOOTNODE allpeer=${ALLPEERS}"
+echo "beacon node with bootnode=$BOOTNODE allpeer=${ALLPEERS} and EXECUTE=$EXECUTE"
 
 sleep 5 && /usr/bin/beacon-chain \
 	--datadir=beacondata \
@@ -12,9 +12,8 @@ sleep 5 && /usr/bin/beacon-chain \
 	--chain-id=${CHAIN_ID:-32382} \
 	--rpc-host=0.0.0.0 \
 	--grpc-gateway-host=0.0.0.0 \
-	--execution-endpoint=http://execute:8551 \
+	--execution-endpoint=http://${EXECUTE}:8551 \
 	--accept-terms-of-use \
-	--verbosity=debug \
 	--p2p-static-id=true \
 	--jwt-secret=/root/config/jwtsecret \
 	--suggested-fee-recipient=0x123463a4b065722e99115d6c222f267d9cabb524 \
