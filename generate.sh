59aacd599b7b5e3bd96719e40e062f604dc0fd10
luxq <xueqian1991@gmail.com>
2023-12-23 23:30:00 +0800

    support any node count

--- generate.sh
+++ generate.sh
@@ -2,14 +2,13 @@
 total_validators=${1:-"64"}
 node_count=${2:-"16"}
 
-
-check=$(($total_validators % $node_count))
-if [ "$check" != 0 ];then
-	echo "invalid param, please check and execute scripte again"
-	exit 1
+# check node count little than total validators
+if [ "$node_count" -gt "$total_validators" ];then
+  echo "invalid param, node count need little than total validators"
+  exit 1
 fi
+
 echo "total validators $total_validators and node count $node_count"
-step=$(($total_validators/$node_count))
 
 file=docker-compose.yml
 rm -f $file
@@ -18,11 +17,21 @@ echo 'version: "3.9"' >> $file
 echo '' >> $file
 echo 'services:' >> $file
 
+function calcpage()
+{
+  index=$1
+  step=$((${total_validators}/${node_count}))
+  startindex=$(($index*$step))
+  endindex=$(($startindex+$step-1))
+  if [ $index -eq $(($node_count-1)) ];then
+    endindex=$(($total_validators-1))
+  fi
+  validatorsnum=$(($endindex-$startindex+1))
+}
 
 for idx in `seq 1 $node_count`
 do
-	num=$step
-	index=$(( $num * ($idx-1)))
+
 #execution config
 geth_rpc_port=$((1000+$idx))
 geth_grpc_port=$((2000+$idx))
@@ -69,15 +78,26 @@ echo "      - execute${idx}" >> $file
 echo "    networks:" >> $file
 echo "      meta:" >> $file
 echo "        ipv4_address: 172.99.1.${idx}" >> $file
-
 echo "" >> $file
+
+# validators config
+
+  index=$(( $idx - 1))
+  step=$((${total_validators}/${node_count}))
+  startindex=$(($index*$step))
+  endindex=$(($startindex+$step-1))
+  if [ $index -eq $(($node_count - 1)) ];then
+    endindex=$(($total_validators-1))
+  fi
+  validatorsnum=$(($endindex-$startindex+1))
+
 echo "  validators${idx}:" >> $file
 echo '    image: ethnet/tools' >> $file
 echo "    container_name: validators${idx}" >> $file
 echo '    entrypoint: /usr/local/bin/validator.sh' >> $file
 echo '    environment:' >> $file
-echo "      - VALIDATORS_NUM=$num" >> $file
-echo "      - VALIDATORS_INDEX=$index" >> $file
+echo "      - VALIDATORS_NUM=${validatorsnum}" >> $file
+echo "      - VALIDATORS_INDEX=${startindex}" >> $file
 echo "      - BEACONRPC=beacon${idx}:4000" >> $file
 echo '    deploy:' >> $file
 echo '      restart_policy:' >> $file
